14bbe76e03c453d80feb0c1d7d717d8a69fd498d
David Ho <1073113@gmail.com>
2023-12-29 15:16:53 +0800

    Reduce pool size and remove migration in DB.ts
    The database connection pool size has been decreased from 5 to 1 to enhance resource efficiency. The migration function has also been commented out, which illustrates a move towards managing database migrations externally. Besides, a console log has been appended for possible troubleshooting requirements.

--- src/lib/DB.ts
+++ src/lib/DB.ts
@@ -1,16 +1,17 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from '../../migrations/schema'
-import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import { Env } from './Env.mjs'
 
 const connectionString: string | undefined = Env.DATABASE_URL
 
-const client = postgres(connectionString, { max: 5 }) // Increase pool size
-export const db = drizzle(client, { schema })
+const client = postgres(connectionString, { max: 1 })
+const db = drizzle(client, { schema })
 
 // Disable migrate function if using Edge runtime for local environment and use `drizzle-kit push` instead
-if (process.env.NODE_ENV !== 'production') {
-  await migrate(db, { migrationsFolder: './migrations' })
-  await client.end()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   console.log('are you running migration?')
+//   await migrate(db, { migrationsFolder: './migrations' })
+// }
+
+export default db
